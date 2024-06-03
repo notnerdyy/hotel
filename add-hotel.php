e6f4c0ecc28cac7202fa5b565a54eddad5d0d50b
ry <rouying0126@gmail.com>
2024-06-03 22:49:08 +0800

    新增旅館資訊

---
+++ add-hotel.php
@@ -0,0 +1,150 @@
+<?php
+require_once("../hotel_db_connect.php");
+
+// 房型下拉選單
+$sql = "SELECT id, room_type FROM room_category";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  $room_types = $result->fetch_all(MYSQLI_ASSOC);
+} else {
+  $room_types = [];
+}
+
+// 地區下拉選單
+$sql2 = "SELECT id, location FROM area_category";
+$result2 = $conn->query($sql2);
+
+if ($result2->num_rows > 0) {
+  $locations = $result2->fetch_all(MYSQLI_ASSOC);
+} else {
+  $locations = [];
+}
+
+$conn->close();
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>creat-user</title>
+
+  <style>
+    .form-group textarea {
+      resize: none;
+      height: 200px;
+    }
+
+    .image-preview {
+      display: flex;
+      gap: 10px;
+      margin-top: 10px;
+    }
+
+    .image-preview img {
+      width: 100px;
+      height: 100px;
+    }
+  </style>
+
+  <?php include("../css.php") ?>
+</head>
+
+<body>
+  <div class="container">
+    <div class="py-2">
+      <a class="btn btn-dark" href="hotel-list.php"><i class="fa-solid fa-arrow-left"></i> 回狗狗旅館列表</a>
+    </div>
+
+    <form action="doAddHotel.php" method="post">
+
+      <div class="form-group mb-3">
+        <label for="name">旅館名稱</label>
+        <input type="text" class="form-control" id="name" name="name" required>
+      </div>
+
+      <div class="form-floating pb-3">
+        <textarea class="form-control" placeholder="Leave a comment here" id="description" name="description" style="height: 100px"></textarea>
+        <label for="description">旅館介紹</label>
+      </div>
+
+      <div class="form-group mb-3 pb-3">
+        <label for="images">圖片</label>
+        <input type="file" class="form-control" id="images" name="images[]" multiple accept="image/*" onchange="previewImages()">
+
+        <label for="images">預覽圖片</label>
+        <div class="image-preview" id="imagePreview"></div>
+      </div>
+
+      <div class="form-group mb-3">
+        <label for="room_type">房間類型</label>
+        <select class="form-select" id="room_type" name="room_type" aria-label="房間類型">
+          <?php foreach ($room_types as $room_type) : ?>
+            <option value="<?= $room_type['id'] ?>"><?= $room_type['room_type'] ?></option>
+          <?php endforeach; ?>
+        </select>
+
+
+      </div>
+      <div class="form-group mb-3">
+        <label for="location">縣市</label>
+        <select class="form-select" id="location" name="location" aria-label="市">
+          <?php foreach ($locations as $location) : ?>
+            <option value="<?= $location['id'] ?>"><?= $location['location'] ?></option>
+          <?php endforeach; ?>
+        </select>
+
+
+      </div>
+      <div class="form-group mb-3">
+        <label for="address">詳細地址</label>
+        <input type="text" class="form-control" id="address" name="address" required>
+      </div>
+
+      <div class="form-group mb-3">
+        <label for="phone">聯絡電話</label>
+        <input type="text" class="form-control" id="phone" name="phone" required>
+      </div>
+
+      <button type="submit" class="btn btn-dark">新增旅館</button>
+    </form>
+  </div>
+
+</body>
+
+
+
+<script>
+  function previewImages() {
+    const preview = document.getElementById('imagePreview');
+    preview.innerHTML = '';
+    const files = document.getElementById('images').files;
+
+    if (files.length > 3) {
+      alert("最多上傳3張圖片");
+      document.getElementById('images').value = '';
+      return;
+    }
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+
+      reader.onload = function(e) {
+        const img = document.createElement('img');
+        img.src = e.target.result;
+        preview.appendChild(img);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+</script>
+
+
+</html>
---
+++ doAddHotel.php
@@ -0,0 +1,46 @@
+<?php
+require_once("../hotel_db_connect.php");
+
+if (!isset($_POST["name"]) || !isset($_POST["description"]) || !isset($_POST["address"]) || !isset($_POST["phone"]) || !isset($_POST["room_type"]) || !isset($_POST["location"])) {
+  echo "請依循正常管道進入此頁";
+  exit;
+}
+
+$name = $_POST["name"];
+$description = $_POST["description"];
+$room_type_id = $_POST["room_type"]; // 下拉式選單, 此資料表為：room_category
+$location_id = $_POST["location"]; // 下拉式選單, 此資料表為：area_category
+$images = $_POST["images"]; // 假設多個圖片用數組傳遞, 此資料表為：hotel_img
+$address = $_POST["address"];
+$phone = $_POST["phone"];
+
+if (empty($name) || empty($description) || empty($address) || empty($phone)) {
+  echo "資訊未填寫完整";
+  exit;
+}
+
+$now_date = date('Y-m-d H:i:s');
+
+// 插入 hotel_list 資料
+$sql = "INSERT INTO hotel_list (name, description, address, phone, created_at, room_type_id, location_id, valid)
+VALUES ('$name', '$description', '$address', '$phone', '$now_date', '$room_type_id', '$location_id', 1)";
+
+if ($conn->query($sql) === TRUE) {
+  $hotel_id = $conn->insert_id;
+
+  // 插入 hotel_img 資料
+  foreach ($images as $path) {
+    $sql_img = "INSERT INTO hotel_img (hotel_id, path, valid) VALUES ('$hotel_id', '$path', 1)";
+    if ($conn->query($sql_img) !== TRUE) {
+      echo "錯誤: " . $sql_img . "<br>" . $conn->error;
+    }
+  }
+
+  echo "新增旅館成功, id為 $hotel_id";
+} else {
+  echo "錯誤: " . $sql . "<br>" . $conn->error;
+}
+
+header("location: hotel-list.php?");
+
+$conn->close();
